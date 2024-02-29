f7d157ca45ce511c2eaefb3a22323db59cf97746
ayo akeni <ayoakeni64@gmail.com>
2024-02-29 09:34:12 +0100

    added auto scroll

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
   spaceBetween: 40,
   cssMode: true,
+  autoplay:{delay: 3000},
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
