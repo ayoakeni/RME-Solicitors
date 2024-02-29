f6f7a601bcf71350a6e3e3cf1a144ecf01788b93
ayo akeni <ayoakeni64@gmail.com>
2024-02-29 02:13:58 +0100

    First commit

---
+++ script/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 40,
+  cssMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
