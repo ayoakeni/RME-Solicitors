cfdda2c446adf6b77d677c5118e0d18d07759b32
ayo akeni <ayoakeni64@gmail.com>
2024-03-24 23:11:13 +0100

    update

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
 // Loading Screen
 window.addEventListener('load', function() {
   setTimeout(function() {
-    // Hide the loading screen
-    document.querySelector('.loading-screen').style.opacity = '0';
-    // Content has finished loading, remove the loading-screen index
-    document.querySelector('.loading-screen').style.zIndex = '-1';
+    // Hide the loading screen and  Remove the loading-screen index when content has finished loading
+    let loadIndex = document.querySelector('.loading-screen');
+    loadIndex.style.opacity = '0';
+    loadIndex.style.zIndex = '-1';
     // Show the content
     document.querySelector('.content').style.opacity = '1';
   }, 2000); // Delay in milliseconds (2000ms = 2 seconds)
